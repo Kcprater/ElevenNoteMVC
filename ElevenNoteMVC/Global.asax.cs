0d170788187b7c1b4fe1af73092fec66d3ae512a
kcprater <kristopherprater@gmail.com>
2020-08-19 14:50:48 -0400

    finished star

--- ElevenNoteMVC/Global.asax.cs
+++ ElevenNoteMVC/Global.asax.cs
@@ -14,6 +14,7 @@ namespace ElevenNoteMVC
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            WebApiConfig.Register();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
---
+++ ElevenNoteMVC/Migrations/202008191812088_Stars.cs
@@ -0,0 +1,16 @@
+namespace ElevenNoteMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Stars : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
