36681509f33b6eaab6d893e158c769c3ae3d8c11
kcprater <kristopherprater@gmail.com>
2020-08-19 09:33:20 -0400

    Add project files.

---
+++ ElevenNote.Data/Note.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    public class Note
+    {
+        [Key]
+        [Display(Name = "Note Id")]
+        public int NoteId { get; set; }
+        [Required]
+        [Display(Name = "Owner Id")]
+        public Guid OwnerId { get; set; }
+        [Required]
+        [Display(Name ="Title")]
+        public string Title { get; set; }
+        [Required]
+        [Display(Name = "Content")]
+        [MaxLength(1000, ErrorMessage = "There are too many characters in this field.")]
+        public string Content { get; set; }
+
+        [Required]
+        [Display(Name = "Time Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+        [Display(Name = "Time Modified")]
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
